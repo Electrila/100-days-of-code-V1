744a250cf05b1afa7d9f1017e10774f5edec32eb
Electrila <48378396+Electrila@users.noreply.github.com>
2020-03-05 16:29:54 -0500

    Upload progress on twitter_cloud.py

---
+++ Twitter Cloud/twitter_cloud.py
@@ -0,0 +1,33 @@
+import re
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os import path
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from PIL import Image
+import GetOldTweets3 as got
+
+# Parameters used to fetch tweets
+# Dates must be in yyyy-mm-dd format
+username = 'realDonaldTrump'
+date_from = '2019-01-01'
+date_until = '2020-03-03'
+count = 100
+
+# Create object for search criteria
+tweet_criteria = got.manager.TweetCriteria().setUsername(username)\
+                                            .setSince(date_from)\
+                                            .setUntil(date_until)\
+                                            .setMaxTweets(count)
+
+# Obtain tweets and convert returned text into DataFrame
+tweets_df = pd.DataFrame([[tweet.text] for tweet in got.manager.TweetManager.getTweets(tweet_criteria)], columns=['text'])
+
+# Import and add to pre-loaded list of stopwords (if necessary)
+stopwords = set(STOPWORDS)
+
+# wordcloud = WordCloud(stopwords=stopwords).generate(tweets_df)
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis('off')
+# plt.show()
