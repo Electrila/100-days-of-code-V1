26949f50334c845ffdc929994a3180d94f866ada
Electrila <48378396+Electrila@users.noreply.github.com>
2020-03-25 09:11:09 -0400

    .py social media banner re-do

---
+++ info_banner.py
@@ -0,0 +1,15 @@
+def retrieve_info():
+    about = {
+        "name": "Megann Comstock",
+        "age": 29,
+        "major": "Computer Science"
+        }
+
+    social = {
+        "twitter": "@electrila",
+        "github": "github.com/Electrila",
+        "DEV": "@electrila"
+        }
+
+    return f"About Me: {about} \nFind Me On: {social}"
+
