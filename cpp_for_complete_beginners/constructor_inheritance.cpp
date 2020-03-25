74a5c3d4c349cb4f7d9e7a2de33a609a337451bf
Electrila <48378396+Electrila@users.noreply.github.com>
2020-03-25 19:06:18 -0400

    Add constructor_inheritance.cpp

---
+++ cpp_for_complete_beginners/constructor_inheritance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Machine {
+private:
+	int id;
+
+public:
+	Machine(): id(0) { cout << "Machine no-argument constructor called." << endl; }
+	Machine(int id): id(id) { cout << "Machine single-argument constructor called." << endl; }
+	void info() { cout << "ID: " << id << endl; }
+};
+
+class Vehicle: public Machine {
+public:
+	Vehicle() { cout << "Vehicle no-argument constructor called." << endl; }
+	Vehicle(int id): Machine(id) { cout << "Vehicle single-argument constructor called." << endl; }
+};
+
+class Car: public Vehicle {
+public:
+	Car(): Vehicle(999) { cout << "Car no-argument constructor called." << endl; }
+};
+
+int main() {
+
+	Car car;
+	car.info();
+
+	return 0;
+}
