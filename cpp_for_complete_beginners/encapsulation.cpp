b37a577928405ce21c0bc92ebad383b3b2e8b63c
Electrila <48378396+Electrila@users.noreply.github.com>
2020-03-25 19:06:46 -0400

    Add encapsulation.cpp

---
+++ cpp_for_complete_beginners/encapsulation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Frog {
+private:
+	string name;
+	string get_name() { return name; }
+
+public:
+	Frog(string name): name(name) {}
+	void info() { cout << "My name is: " << get_name() << endl; }
+};
+
+int main() {
+
+	Frog frog("Freddy");
+
+	frog.info();
+
+	return 0;
+}
