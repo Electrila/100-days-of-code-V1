386efea7217218d50cb17a313358868fb441a3ce
Electrila <48378396+Electrila@users.noreply.github.com>
2020-03-27 22:22:51 -0400

    Uploaded prefix_and_postfix.cpp

---
+++ cpp_for_complete_beginners/prefix_and_postfix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int value = 8;
+
+	cout << value++ << endl;
+	cout << value << endl;
+
+	int number = ++value;
+
+	cout << number << endl;
+
+	return 0;
+}
